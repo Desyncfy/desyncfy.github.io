29737784d8416916a5dc377e26c5e45b6836cd3c
desyncfy <chickenmanfy@proton.me>
2024-05-20 15:59:35 -0700

    Tailwind + Navbar for blog homepage

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// TODO: write sort fuction for sorting blog cards
+function sort(type) {
+    if (type == "code") {
+        $( "div" ).remove( ".code" );
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./blog/**/*.{html,js}","**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
