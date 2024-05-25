dadd363b9d91f2dc22539e4ee5d9c04aad0892e8
desyncfy <chickenmanfy@proton.me>
2024-05-25 16:04:41 -0700

    Add first page to blog

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 // TODO: write sort fuction for sorting blog cards
 function sort(type) {
     if (type == "code") {
+        $( "div" ).remove( ".other" );
+    } else {
         $( "div" ).remove( ".code" );
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./blog/**/*.{html,js}","**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ubuntu': ['Ubuntu'],
+        'inter': ['inter'],
+        'jetbrains': ['Jetbrains Mono'],
+      },
+      colors: {
+        'element': '#569CD6',
+        'arg': '#9CDCFE',
+        'string': '#CE9178',
+        'greaterless': '#808080',
+        'brackets': '#F8D704',
+      }
+    },
   },
   plugins: [],
 }
