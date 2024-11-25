387d8dccc4fb1ccfbf6da9fdbc2e95363b6b2724
Desyncfy <chickenmanfy@proton.me>
2024-11-24 20:20:14 -0800

    base site

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-// TODO: write sort fuction for sorting blog cards
-function sort(type) {
-    if (type == "code") {
-        $( "div" ).remove( ".other" );
-    } else {
-        $( "div" ).remove( ".code" );
-    }
-}
