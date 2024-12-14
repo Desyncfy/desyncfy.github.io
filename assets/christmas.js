04cf4241f44f0980aea11175517c0b411ba3fd7c
Desyncfy <chickenmanfy@proton.me>
2024-12-14 14:49:49 -0800

    error

--- assets/christmas.js
+++
@@ -1,6 +0,0 @@
-  document.addEventListener('onkeydown', (e) => {
-    if (e.key === 'w' || e.key === 'up') {
-      document.body.style.backgroundColor = 'red';
-    }
-    console.log(e.key);
-  })
